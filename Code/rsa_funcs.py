9edd7bbfbc1d1b536668eabd5591b3aaa77cf44f
Alicia L Bergeron <bergerar@l001.cm.cluster>
2022-09-22 14:12:46 -0400

    housekeeping

--- Code/rsa_funcs.py
+++ Code/rsa_funcs.py
@@ -16,29 +16,44 @@ from scipy.spatial.distance import squareform
     
 
 def fit_rsa(rdm_data,rdm_model):
+    # Takes in two similarity matrices (n x n), has to be square, symmetrical, and either 1s or 0s on the diagonal
+    # vectorizes them 
+    # Calculates a correlation between them
+    # returns r value (between -1 and 1)
+    
     return np.corrcoef(get_triu(rdm_data),get_triu(rdm_model))[0,1]
         
 def make_RDM(inVec,data_scale='ratio',metric='euclidean'):
-    vec = inVec
-    vec = (vec - min(vec.flatten())) / (max(vec.flatten())-min(vec.flatten()))
+    # Makes an similarity matrix
+    # Takes in a data matrix (n x p), subjects as rows, and abitrary number of features 
+    # Whether you give it a 5x2 or a 5x10000 matrix, will give back a 5x5 similarity matrix
+    # 
+    vec = inVec # Takes whatever data you give it 
+    vec = (vec - min(vec.flatten())) / (max(vec.flatten())-min(vec.flatten())) # Normalizes between 0 and 1
     
     if np.ndim(inVec)==1: # must be at least 2D
-        vec = np.vstack((vec,np.zeros(vec.shape))).transpose()
+        vec = np.vstack((vec,np.zeros(vec.shape))).transpose() # this is... just... magic. so beautiful. I mean really. 
+        # ^ ok ok lemme explain 
+        # pdist function below, works on >=2D data
+        # this is a hack in case you want to use it on 1D data
+        # in case you pass it a 1D vector, it adds a column of zeros
+        # which doesnt change the distances, 
                    
-    mat = squareform(pdist(vec,metric=metric).transpose())
+    mat = squareform(pdist(vec,metric=metric).transpose()) # Takes in calcs the distances, turns into a dissimilarity matrix
 
-    if data_scale=='ordinal':
+    if data_scale=='ordinal': # In case the data is ordinal, makes into a same or differnt matrix or ones and zeros
         mat[mat!=0]=1 # Make into zeros and ones
         
     return mat
 
 def get_triu(inMat):
-
-    assert np.ndim(inMat)==2, 'not 2 dim, wtf'
-    assert inMat.shape[0]==inMat.shape[1], 'not a square'
+    # Nifty function that takes in a similarity matrix (nxn) and vectorizes it (returns the upper triangle values)
+    
+    assert np.ndim(inMat)==2, 'not 2 dim, wtf' # Check if some dummy passed a nd array instead of a simmmat
+    assert inMat.shape[0]==inMat.shape[1], 'not a square' # make sure it's a square
 
     n = inMat.shape[0]
-    triu_vec = inMat[np.triu_indices(n=n,k=1)]
+    triu_vec = inMat[np.triu_indices(n=n,k=1)] # Takes the upper triangle values
     
     return triu_vec
 
